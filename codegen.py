378ef280c99b65f0d0dd0e50abbc1e8de815f61e
Artem <Artem@noname.null>
2019-12-08 11:35:36 +0300

    Каркас генерации кода завершен. Нужна работа с типами. Вставляем очередной костыль для подписи типов к id

--- codegen.py
+++ codegen.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 def intro(tree):
     return f"""
-    #include "stdio.h"
-    void main() {
-    {codegen_recursive(tree)}
-    }
+#include "stdio.h"
+    
+void main() {{
+{codegen_recursive(tree)}
+return 0;
+}}
 """
 
 def codegen_recursive(tree):
@@ -12,19 +14,30 @@ def codegen_recursive(tree):
     for node in tree[1]:
         if isinstance(node, str):
             continue
-        elif node[0] not in ['DIM', 'IO']:
+        elif node[0] not in ['DIM', 'IO', 'IF', 'LOOP', 'EXPRESSION']:
             codestr += codegen_recursive(node)
         else:
             if node[0] == 'DIM':
                 codestr += dim(node)
             if node[0] == 'IO':
                 codestr += io(node)
+            if node[0] == 'IF':
+                codestr += ifs(node)
+            if node[0] == 'LOOP':
+                codestr += loop(node)
+            if node[0] == 'EXPRESSION':
+                codestr += expression(node)
+                
     return codestr
 
 def dim(dim_node):
-    types = 'char[80]' if dim_node[1][3] == 'string' else 'int'
     ids = dim_node[1][1]
-    return f"""{types} {ids}; \n"""
+    if dim_node[1][3] == 'string':
+        return f'char {ids}[80]; \n'''
+    elif dim_node[1][3] == 'int':
+        return f'int {ids} = 0; \n'
+    
+    
 def io(io_node):
     # если вывод
     if len(io_node[1]) == 2:
@@ -35,6 +48,21 @@ def io(io_node):
         ids = io_node[1][0]
         return f'scanf("%d?", &{ids}); \n'
 
+def condition(cond_node):
+    # убираем тип
+    cond_node[1][-1] = cond_node[1][-1].split('|')[0]
+    return ' '.join(cond_node[1])
 
+def ifs(if_node):
+    if len(if_node[1]) == 8:
+        return f'if ({condition(if_node[1][1])}) {{\n{codegen_recursive(if_node[1][3])} }} \nelse \n{{ \n{codegen_recursive(if_node[1][6])}}}'
+    else:
+        return f'if ({condition(if_node[1][1])}) {{\n{codegen_recursive(if_node[1][3])}}}'
 
-print(str(intro(stree)))
+def loop(while_node):
+    return f'while ({condition(while_node[1][1])}) {{\n{codegen_recursive(while_node[1][3])}}}'
+
+def expression(expr_node):
+    return f'{" ".join(expr_node[1])}; \n'
+
+print(intro(stree))
